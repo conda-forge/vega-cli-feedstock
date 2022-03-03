b3874e6da8e9d1c28d540486e7112e4878ba3e7b
Uwe L. Korn <uwe.korn@quantco.com>
2022-03-03 14:19:30 +0100

    Don't use pre-built binaries

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+    export npm_config_arch="arm64"
+fi
+# Don't use pre-built gyp packages
+export npm_config_build_from_source=true
+
+rm $PREFIX/bin/node
+ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
+
 mkdir -p $PREFIX/lib/vega-cli
 cd $PREFIX/lib/vega-cli
 yarn add vega-cli@$PKG_VERSION
