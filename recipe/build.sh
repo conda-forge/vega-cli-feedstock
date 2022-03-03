b2727f7df6ec7a7062a6dd5f020db8a7c86ce578
Uwe L. Korn <uwe.korn@quantco.com>
2022-03-03 15:01:01 +0100

    Add -fpermissive

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 if [[ "${target_platform}" == "osx-arm64" ]]; then
-    export npm_config_arch="arm64"
+  export npm_config_arch="arm64"
 fi
 # Don't use pre-built gyp packages
 export npm_config_build_from_source=true
+
+if [[ "${target_platform}" == linux-* ]]; then
+  # We need to be more permissive to get the code compiling
+  export CFLAGS="${CFLAGS} -fpermissive"
+  export CXXFLAGS="${CXXFLAGS} -fpermissive"
+fi
 
 rm $PREFIX/bin/node
 ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
